f667d3143df34a26904738e66a6cd4b7241e4644
astarostap <abrahamstarosta1@DN0a227743.SUNet>
2015-12-03 15:24:52 -0800

    merged

--- Siembra/MainViewController.swift
+++ Siembra/MainViewController.swift
@@ -11,9 +11,31 @@ import CoreData
 
 class MainViewController: UITabBarController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Tab bar appearance
         launchDatabase()
+        let appearance = UITabBar.appearance()
+        appearance.tintColor = UIColor(netHex:0xD5AFFF)
+        appearance.barTintColor = UIColor.blackColor()
+        
+        let tabBarController :UITabBarController  = self
+        
+        let tabBar :UITabBar = tabBarController.tabBar
+        let tabBarItem1 :UITabBarItem = (tabBar.items![0]) //search
+        let tabBarItem2 :UITabBarItem = (tabBar.items![1]) //home
+        let tabBarItem3 :UITabBarItem = (tabBar.items![2]) //you
+        
+        let searchImage: UIImage = UIImage(named: "rsz_2search.png")!
+        tabBarItem3.image = searchImage
+        
+        let homeImage: UIImage = UIImage(named: "rsz_home.png")!
+        //tabBarItem2.image = homeImage.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        tabBarItem1.image = homeImage
+        
+        let heartImage: UIImage = UIImage(named: "rsz_heart.png")!
+        tabBarItem2.image = heartImage
         // Do any additional setup after loading the view.
     }
     
